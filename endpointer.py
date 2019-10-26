f4000fba861ebbde1fe6ffc786de5a5b5ca11cc2
thebabush <paolo.montesel+gh@gmail.com>
2019-10-26 19:59:06 +0200

    fix endpointer

--- endpointer.py
+++ endpointer.py
@@ -4,14 +4,45 @@ import numpy as np
 import scipy.io.wavfile
 import matplotlib.pyplot as plt
 
+import sys
+
+
+if len(sys.argv) < 2:
+    sys.argv.append('sample.wav')
+
+
+duration = 10
+if len(sys.argv) >= 3:
+    duration = int(sys.argv[2])
+
+
 tf.enable_eager_execution()
 
+
 # read the wav file
-samplerate,rawdata = scipy.io.wavfile.read('sample.wav')
-input_buffer_full = rawdata[:,0]
+samplerate, rawdata = scipy.io.wavfile.read(sys.argv[1])
+
+assert samplerate == 16000
+
+# chop the file
+rawdata = rawdata[:samplerate * duration]
+
+# handle both mono and stereo files
+if len(rawdata.shape) == 2:
+    input_buffer_full = rawdata[:,0]
+else:
+    input_buffer_full = rawdata[:]
+
+# normalize (int16_t => float32)
+input_buffer_full = input_buffer_full * 0.000030518
+
 signals = tf.reshape(input_buffer_full.astype(np.float32), [1,-1])	# probably two channels, 0=left,1=right
-samples_per_10ms = samplerate /100
-samples_per_25ms = samplerate /40
+samples_per_10ms = samplerate//100
+samples_per_25ms = samplerate//40
+
+# TODO: remove noise
+
+# TODO: use these values are used to prefilter the signal
 fband = [125.,7500.]
 channels_ep = 40
 stfts = tf.signal.stft(signals, frame_length=samples_per_25ms, frame_step=samples_per_10ms)
@@ -19,7 +50,17 @@ spectrograms = tf.abs(stfts)
 
 # Warp the linear scale spectrograms into the mel-scale.
 num_spectrogram_bins = stfts.shape[-1].value
-linear_to_mel_weight_matrix_40 = tf.signal.linear_to_mel_weight_matrix(channels_ep, num_spectrogram_bins, samplerate, fband[0], fband[1])
+
+# Confirmed freq values
+mel_lo = 125
+mel_hi = 3800
+linear_to_mel_weight_matrix_40 = tf.signal.linear_to_mel_weight_matrix(
+    channels_ep,
+    num_spectrogram_bins,
+    samplerate,
+    mel_lo,
+    mel_hi,
+)
 mel_spectrograms_40 = tf.tensordot(spectrograms, linear_to_mel_weight_matrix_40, 1)
 mel_spectrograms_40.set_shape(spectrograms.shape[:-1].concatenate(linear_to_mel_weight_matrix_40.shape[-1:]))
 
@@ -44,18 +85,18 @@ print('outputs',output_details)
 list_a = []
 list_b = []
 for filterbank_energies_ep in log_mel_spectrograms_40[0]:
-	# run the endpointer to decide if we should run the RNN
-	input_data_ep = [filterbank_energies_ep]
-	interpreter.set_tensor(input_details[0]['index'], input_data_ep)
-	interpreter.invoke()
-	output_data_ep = interpreter.get_tensor(output_details[0]['index'])
+    # run the endpointer to decide if we should run the RNN
+    input_data_ep = [filterbank_energies_ep]
+    interpreter.set_tensor(input_details[0]['index'], input_data_ep)
+    interpreter.invoke()
+    output_data_ep = interpreter.get_tensor(output_details[0]['index'])
 
-	[[a,b]] = output_data_ep
-	list_a.append(a)
-	list_b.append(b)
+    [[a,b]] = output_data_ep
+    list_a.append(a)
+    list_b.append(b)
 
-	print(input_data_ep)
-	print(output_data_ep)
+    print(input_data_ep)
+    print(output_data_ep)
 
 
 f, (wav,plota,plotb) = plt.subplots(3, sharex=True)
